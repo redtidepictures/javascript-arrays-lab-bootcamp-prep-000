a4530ceb4b3321f37c9583db11ba55e2bdc0b34a
Tyler Wales <walestyl@gmail.com>
2017-10-13 07:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-   var newArray = kittens.slice(3);
+   var newArray = kittens.slice(0, kittens.length - 1);
    return newArray
 }
